5ccc72e0d82ec1cbedc136cba276d2c0570663b5
Zakhirah <100054724+PatelShakil@users.noreply.github.com>
2022-10-13 11:27:51 +0530

    added uploadactivity

--- app/src/main/java/com/talaba/zakhirah/MainActivity.kt
+++ app/src/main/java/com/talaba/zakhirah/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.talaba.zakhirah
 
+import android.content.Intent
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.talaba.zakhirah.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +18,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
+        menuInflater.inflate(R.menu.menu,menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_upload ->
+                startActivity(Intent(this,UploadKitabActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
     override fun onBackPressed() {
         finishAffinity()
---
+++ app/src/main/java/com/talaba/zakhirah/UploadKitabActivity.kt
@@ -0,0 +1,11 @@
+package com.talaba.zakhirah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadKitabActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_kitab)
+    }
+}
